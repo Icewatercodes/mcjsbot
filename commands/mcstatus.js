7013ed36eaf9f169f0a709a396e2a895c8cc1480
icewatercodes <williamjohannsen5@gmail.com>
2026-08-17 17:24:17 +0200

    Added playlist to mcstatus and made it ephemeral

--- commands/mcstatus.js
+++ commands/mcstatus.js
@@ -6,22 +6,28 @@ module.exports = {
         .setDescription('Point the Bot to your mc ip/domain'),
     async execute(interaction) {
 
-        const sent = await interaction.reply({ content: 'Checker Lige...', withReponse: true})
+        const sent = await interaction.reply({ content: 'Checker Lige...', withReponse: true, ephemeral: true});
 
-        //const getstatus = await fetch(`https://api.mcsrvstat.us/3/${process.env.MC_IP}`);
-        const getstatus = await fetch(`https://api.mcstatus.io/v2/status/java/${process.env.MC_IP}`)
-        const mcstatus = await getstatus.json()
+        const getstatus = await fetch(`https://api.mcstatus.io/v2/status/java/${process.env.MC_IP}`);
+        const mcstatus = await getstatus.json();
 
-        console.log("\n" + JSON.stringify(mcstatus, null, 2) + "\n");
+        let reply = 
+            `OTG Minecraft server status:  ${mcstatus.online ? "Online" : "Offline"}\n` +
+            `IP:  ${mcstatus.host}\n` +
+            `Spiller:  ${mcstatus.players.online} / ${mcstatus.players.max}\n` +
+            `Spillere:\n`
+        ;
+
+        for(i = 0; i < mcstatus.players.online; i++) {
+            reply += mcstatus.players.list[i].name_clean + '\n';
+        
+        }
+            
         if (!mcstatus.online) {
             await interaction.editReply("OTH Minecraft server status: Offline");
             return;
         }else {
-            await interaction.editReply(`
-                OTG Minecraft server status: ${mcstatus.online ? "Online" : "Offline"}
-                IP: ${mcstatus.host}
-                Spiller: ${mcstatus.players.online} / ${mcstatus.players.max}
-            `);
+            await interaction.editReply({content: reply, ephemeral: true});
         }
     },
 };
--- commands/mcstatusjson.js
+++ commands/mcstatusjson.js
@@ -6,13 +6,13 @@ module.exports = {
         .setDescription('mcstatus i fuld json'),
     async execute(interaction) {
 
-        const sent = await interaction.reply({ content: '...', withReponse: true})
+        const sent = await interaction.reply({ content: '...', withReponse: true, ephemeral: true})
 
         const getstatus = await fetch(`https://api.mcstatus.io/v2/status/java/${process.env.MC_IP}`)
         const mcstatus = await getstatus.json()
 
-        console.log("\n" + JSON.stringify(mcstatus, null, 2) + "\n");
+        //console.log("\n" + JSON.stringify(mcstatus, null, 2) + "\n");
 
-        await interaction.editReply(JSON.stringify(mcstatus, null, 2));
+        await interaction.editReply({content:JSON.stringify(mcstatus, null, 2), ephemeral: true});
     }
 }
