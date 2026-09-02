df5ab8bd1bbaddf4583a29a3f5b21c26971e99e2
william <williamjohannsen5@gmail.com>
2026-09-02 10:36:13 +0200

    added weeks

--- commands/skema.js
+++ commands/skema.js
@@ -7,8 +7,10 @@ module.exports = {
     async execute(interaction) {
         const sent = await interaction.reply({ content: 'Finder skema', withResponse: true, ephemeral: true});
 
-        const skemajson = require("../skema.json");
-
+        const week = getWeek(new Date());
+        //console.log(`uge: ${week}`)
+        const skemajson = require(`../uge_${week}.json`);
+        
         var skema = `Ugens skema ${skemajson.classes[0].day} til ${skemajson.classes[skemajson.classes.length-1].day}\n\n`;
 
         const days = [
@@ -22,15 +24,16 @@ module.exports = {
         var nowday = 0;
         if(nowday == undefined) {
             nowday = new Date().getDay()-1;
-            console.log(nowday);
+            
+            //console.log(nowday);
             while(nowday == 5 || nowday == 6) {
                 nowday++;
             }
         }
 
-        console.log(nowday);
+        //console.log(nowday);
         const day = days[nowday];
-        console.log(day);
+        //console.log(day);
         var correctClasses = [];
         for(const k in skemajson.classes) {
             //klass = skemajson.classes[k];
@@ -61,3 +64,17 @@ module.exports = {
         
     },
 };
+
+function getWeek(d) {
+    
+    const dt = new Date(d); // Convert input string to Date object
+    const ys = new Date(dt.getFullYear(), 0, 1); // Get January 1st of the same year
+    const dp = Math.floor((dt - ys) / 86400000); // Calculate the days passed since January 1st (1000 * 60 * 60 * 24 = 86400000)
+    const sw = ys.getDay();
+    const so = (sw === 0) ? 6 : sw - 1;  // Adjust Sunday (0) to 6 (ISO starts Monday)
+    const wn = Math.floor((dp + so) / 7) + 1;
+
+    return wn;
+}
+
+
