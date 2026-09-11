25b63c9666b8ff6a35b1f904f423335455f50661
icewatercodes <williamjohannsen5@gmail.com>
2026-09-11 19:38:45 +0200

    weeksssss

--- commands/skema.js
+++ commands/skema.js
@@ -11,28 +11,42 @@ module.exports = {
             .setMinValue(0)
             .setMaxValue(4)
         )
+        .addIntegerOption(Option =>
+            Option.setName('uge')
+            .setDescription('Hvilken uge (0-52)')
+            .setRequired(false)
+            .setMinValue(0)
+            .setMaxValue(52)
+        )
         .addBooleanOption(option =>
             option.setName('ephemeral')
             .setDescription('Print til kun dig')
             .setRequired(false)
         ),
     async execute(interaction) {
-        const sent = await interaction.reply({ content: 'Finder skema', withResponse: true, ephemeral: true});
-
+        try {
+            var nowday = interaction.options.getInteger('dag') || undefined;
+            var ephe = interaction.options.getBoolean('ephemeral');
+        } catch (error) {
+            var ephe = true;
+            var nowday = undefined;
+        }
+        const sent = await interaction.reply({ content: 'Finder skema', withResponse: true, ephemeral: ephe});
+        
+        
         const skemajson = require("../skema.json");
-
         var skema = `Ugens skema ${skemajson.classes[0].day} til ${skemajson.classes[skemajson.classes.length-1].day}\n\n`;
 
-        const days = [
-            "mandag ",
-            "tirsdag",
-            "onsdag ",
-            "torsdag",
-            "fredag "
-        ];
+            const days = [
+                "mandag ",
+                "tirsdag",
+                "onsdag ",
+                "torsdag",
+                "fredag "
+            ];
 
         if(nowday == undefined) {
-            nowday = new Date().getDay()-1;
+            nowday = new Date().getDay();
             console.log(nowday);
             while(nowday == 5 || nowday == 6) {
                 nowday++;
@@ -41,9 +55,9 @@ module.exports = {
             
         }
 
-        //console.log(nowday);
+        console.log(nowday);
         const day = days[nowday];
-        //console.log(day);
+        console.log(day);
         var correctClasses = [];
         for(const k in skemajson.classes) {
             //klass = skemajson.classes[k];
@@ -52,25 +66,29 @@ module.exports = {
             if(skemajson.classes[k].day.slice(0,7) != day) continue;
             correctClasses.push(k);
 
-        }
-
-        //console.log(`correctClasses[0] = ${correctClasses[0]}`)
-
-        skema += 
-        `***Dag: ${skemajson.classes[correctClasses[0]].day}***\n`;
-        for(const i in correctClasses){
-            //console.log()
-            if(skemajson.classes[correctClasses[i]].time != undefined){
-                skema += `    **Tid:** ${skemajson.classes[correctClasses[i]].time}\n`;
-                skema += `    **Fag:** ${skemajson.classes[correctClasses[i]].course.split(" - ")[2]}\n`;
-                skema += `    **Klasse:** ${skemajson.classes[correctClasses[i]].room}\n`
-                skema += `    **lektier:** ${skemajson.classes[correctClasses[i]].homework_notes}\n\n`
             }
+
+            skema +=
+            `***Dag: ${skemajson.classes[correctClasses[0]].day}***\n`;
+            for(const i in correctClasses){
+                //console.log()
+                if(skemajson.classes[correctClasses[i]].time != undefined){
+                    skema += `    **Tid:** ${skemajson.classes[correctClasses[i]].time}\n`;
+                    skema += `    **Fag:** ${skemajson.classes[correctClasses[i]].course.split(" - ")[2]}\n`;
+                    skema += `    **Klasse:** ${skemajson.classes[correctClasses[i]].room}\n`
+                    skema += `    **lektier:** ${skemajson.classes[correctClasses[i]].homework_notes}\n\n`
+                }
+            }
+
+
+
+
         }
-
-        
-
-        await interaction.editReply({content: skema}); 
-        
+        //send result
+        await interaction.editReply({content: skema});
     },
 };
+
+
+
+
